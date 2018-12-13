f99dbe0d7108e50770ed77318b4e743feb17a4aa
Unknown <lucasjclelland@gmail.com>
2018-12-13 14:46:44 -0500

    Went back to v2 with averaging fErrors
    getting better results as of 12 Nov 2018

--- olt_v2.m
+++ olt_v2.m
@@ -1,53 +1,80 @@
-clear; 
+clear;
+
 %close all;
-FSamp = 5e6;                                    % Sampling rate Hz
-blockSize = floor(FSamp*.001);                  % Samples per block 
-tBlockSize = blockSize/FSamp;                   % Seconds
-simTime = .2;                                   % Seconds
-numBlocks = floor(FSamp*simTime/blockSize);     % Number of simulated blocks
 
+FSamp = 5e6; % Sampling rate Hz
+blockSize = floor(FSamp*.001); % Samples per block
+tBlockSize = blockSize/FSamp; % Seconds
+simTime = 2; % Seconds
+numBlocks = floor(FSamp*simTime/blockSize); % Number of simulated blocks
 %% Generate carrier
-t=[0:1:blockSize*numBlocks-1]/FSamp;            % Seconds
-fCarrier = rand*10000-5000;                     % Hz
-pDeltaCarr=rem(2*pi*fCarrier*t,2*pi);
-carrier = exp(1i*pDeltaCarr);      
-carrier = awgn(carrier,-40,'measured');
-
+t=[0:1:blockSize*numBlocks-1]/FSamp; % Seconds
+%fCarrier = rand*10000-5000; % Hz
+fCarrier=2000;
+pDeltaCarr=rem(2*pi*fCarrier*t,2*pi); %exponent
+carrier = exp(1i*pDeltaCarr); %no noise
+carrier = awgn(carrier,-40,'measured'); %add noise
+%load test;
+%carrier=test;
 %% Initial guess using FFT
-[~,loc]=max(fftshift(fft(carrier)));
-fGuess=-FSamp/2+loc/length(carrier)*FSamp;
-fprintf('Initial error: %d(Hz)\n',fGuess-fCarrier);
+% [~,loc]=max(fftshift(fft(carrier)));
+% fGuess=-FSamp/2+loc/length(carrier)*FSamp;
+
+% [~,loc]=max(fftshift(fft(carrier,2^23))); %Unrealistic fft resolution
+% fGuess=-FSamp/2+loc/(2^23)*FSamp;
+fGuess=fCarrier-200;
+
+%fCarrier=fGuess;
+
+%fGuess=fCarrier+50;
+
+fprintf('Initial guess: %d(Hz)\n',fGuess);
 
 %% Open loop carrier tracking
+
 counter = 1;
+
 while numBlocks>2
     angles = zeros(numBlocks,1);
     I=0;
     Q=0;
-    pDeltaGuess=rem(2*pi*fGuess*t,2*pi); 
+    pDeltaGuess=rem(2*pi*fGuess*t,2*pi);
     for idx=1:numBlocks
-       rxSignal = carrier((idx-1)*blockSize+1:idx*blockSize);
-       localReplica = exp(1i*pDeltaGuess((idx-1)*blockSize+1:idx*blockSize));
-       I_d1 = I; %Previous Block I 
-       Q_d1 = Q; %Previous Block Q
-       tmp = localReplica*rxSignal'; %correlate local with rx
-       I=real(tmp);
-       Q=imag(tmp);
-       if idx>1
-            angles(idx)=fnFreqDiscrim(I_d1,Q_d1,I,Q,tBlockSize,1)/(2*pi); %How does this change anything?
-       end
-      % angles(idx)=atan2(Q,I); %Are these the phase angles?
+        rxSignal = carrier((idx-1)*blockSize+1:idx*blockSize);
+        localReplica = exp(1i*pDeltaGuess((idx-1)*blockSize+1:idx*blockSize));
+        
+        I_d1 = I;
+        Q_d1 = Q;
+        
+        tmp = localReplica*rxSignal';
+        
+        I=real(tmp);
+        Q=imag(tmp);
+        
+        fError=0;
+        if idx>1
+            fError=fnFreqDiscrim(I_d1,Q_d1,I,Q,tBlockSize,1); %atan2
+            %fError=fnFreqDiscrim(I_d1,Q_d1,I,Q,tBlockSize,2); %atan
+        end
+        
+        %angles(idx)=atan2(Q,I);
+        
+        angles(idx)=fError; %Estimated for each block 
+        
     end
-
-    tmp=polyfit((1:1:numBlocks).'*tBlockSize,unwrap(angles),1); % use least squares to find rate of change (rad/s)
-    fError = tmp(1)/(2*pi);
+    
+    %errorfit=polyfit((1:1:numBlocks).'*tBlockSize,unwrap(angles),1); % use least squares to find rate of change (rad/s)
+    
+    meanError=mean(angles(2:numBlocks)); %Mean of angles
+    
+    fError = meanError(1)/(2*pi); % convert from rad/s
     fErrors(counter)=fError;
+    
     fGuess=fGuess-fError;
     fGuesses(counter)=fGuess;
     
-    %   plot results
+    % plot results
     figure (1);
-    
     subplot(4,1,1);
     plot((1:1:counter),fGuesses);
     hold('on');
@@ -65,7 +92,7 @@ while numBlocks>2
     subplot(4,1,3);
     plot((1:1:numBlocks).'*tBlockSize,unwrap(angles));
     hold('on');
-    yFit=polyval(tmp,(1:1:numBlocks).'*tBlockSize);
+    yFit=polyval(meanError,(1:1:numBlocks).'*tBlockSize);
     plot((1:1:numBlocks).'*tBlockSize,yFit);
     xlabel('Time (s)');
     ylabel('Angle (rad)');
@@ -80,11 +107,12 @@ while numBlocks>2
     axis([-10e3,10e3,-80,10]);
     drawnow;
     % end plot results
-    
     counter=counter+1;
     blockSize=blockSize*2;
-    numBlocks = floor(FSamp*simTime/blockSize);          % Number of simulated blocks
-    tBlockSize = blockSize/FSamp;                       % Seconds
+    numBlocks = floor(FSamp*simTime/blockSize); % Number of simulated blocks
+    tBlockSize = blockSize/FSamp; % Seconds
     pause(.5)
+    
 end
-fprintf('Final error: %d(Hz)\n',fGuess-fCarrier);
+
+fprintf('Final guess: %d(Hz)\n',fGuess);
